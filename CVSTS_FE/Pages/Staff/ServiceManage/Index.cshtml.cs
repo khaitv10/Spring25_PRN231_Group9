4cb444bea09742e3012e00653356a189812de8ba
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2025-02-28 05:30:30 +0700

    filter service

--- CVSTS_FE/Pages/Staff/ServiceManage/Index.cshtml.cs
+++ CVSTS_FE/Pages/Staff/ServiceManage/Index.cshtml.cs
@@ -9,6 +9,7 @@ using BOs.Models;
 using System.Net.Http.Headers;
 using BOs.ResponseModels.Child;
 using BOs.ResponseModels.Service;
+using BOs.RequestModels.Service;
 
 namespace CVSTS_FE.Pages.Staff.ServiceManage
 {
@@ -25,9 +26,33 @@ namespace CVSTS_FE.Pages.Staff.ServiceManage
 
         public async Task<IActionResult> OnGetAsync()
         {
+            var query = new ServiceQueryModel
+            {
+                Name = !string.IsNullOrEmpty(Request.Query["Name"]) ? Request.Query["Name"].ToString() : "",
+                Status = bool.TryParse(Request.Query["Status"], out var status) ? (bool?)status : null,
+                FromPrice = decimal.TryParse(Request.Query["FromPrice"], out var fromPrice) ? fromPrice : 0,
+                ToPrice = decimal.TryParse(Request.Query["ToPrice"], out var toPrice) ? toPrice : 99999999.99M,
+            };
+
 
             var client = CreateAuthorizedClient();
-            var url = $"/api/Service/getAllService";
+            var baseUrl = "/api/Service/getAllService";
+
+            var queryParams = new List<string>();
+
+            if (!string.IsNullOrEmpty(query.Name))
+                queryParams.Add($"Name={Uri.EscapeDataString(query.Name)}");
+
+            if (query.Status.HasValue)
+                queryParams.Add($"Status={query.Status.Value}");
+
+            if (query.FromPrice >=0)
+                queryParams.Add($"FromPrice={query.FromPrice}");
+
+            if (query.ToPrice <= 99999999.99M)
+                queryParams.Add($"ToPrice={query.ToPrice}");
+
+            var url = queryParams.Any() ? $"{baseUrl}?{string.Join("&", queryParams)}" : baseUrl;
 
             var response = await APIHelper.GetAsJsonAsync<List<ServiceResponseModel>>(client, url);
 
