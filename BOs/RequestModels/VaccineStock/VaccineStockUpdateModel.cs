3d0a3583c4d874a9155f51089d3d140864528fec
Shymastic <shymastic@gmail.com>
2025-03-22 14:21:23 +0700

    apply BR for vaccineStock

--- BOs/RequestModels/VaccineStock/VaccineStockUpdateModel.cs
+++ BOs/RequestModels/VaccineStock/VaccineStockUpdateModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,23 @@ namespace BOs.RequestModels.VaccineStock
 {
     public class VaccineStockUpdateModel
     {
+        [Required(ErrorMessage = "Quantity is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be a non-negative number.")]
         public int Quantity { get; set; }
 
+        [Required(ErrorMessage = "ExpiryDate is required.")]
+        [FutureDate(ErrorMessage = "ExpiryDate must be a future date.")]
         public DateTime ExpiryDate { get; set; }
-
+    }
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                return date > DateTime.Now;
+            }
+            return false;
+        }
     }
 }
